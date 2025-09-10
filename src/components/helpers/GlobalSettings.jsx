ecf5fca2b4d94499fdb0b802163ea3fd00f39020
cliveyg <junkcat@gmail.com>
2025-09-10 16:23:33 -0300

    Fix bug in CountDownTimer

--- src/components/helpers/GlobalSettings.jsx
+++ src/components/helpers/GlobalSettings.jsx
@@ -20,6 +20,22 @@ export function GlobalProvider({ children }) {
             .set("x-access-token", Cookies.get('access-token'))
             .then((res) => {
                 // handle response
+                console.log(res.status)
+                /*
+                setProfile(res.body)
+                setProfileIcon(getIcon(res.body.standard_avatar))
+                if (res.body.about_me !== null && res.body.about_me !== "") {
+                    setAboutMe(res.body.about_me)
+                }
+                if (res.body.standard_avatar !== null && res.body.standard_avatar !== "") {
+                    setAvatarName(res.body.standard_avatar)
+                    setProfileIcon(getIcon(res.body.standard_avatar))
+                    localStorage.setItem('avatar',res.body.standard_avatar)
+                    setProfileImageString(res.body.standard_avatar)
+                    setAvatarObj(profileImageString)
+                }
+                 */
+                console.log("found profile")
             })
             .catch((err) => {
                 if (err.status === 401) {
